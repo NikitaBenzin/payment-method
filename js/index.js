bc7469b3291ae22a2cccbce4de99d33dc94b3e38
NikitaBenzin <nikitory05@gmail.com>
2024-04-11 23:12:53 +0200

    Update: index.html, index.scss

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/* TODO: 
+  1: check radio inputs (change checked state)
+  2: change bank image
+  3: validate form
+  4: clear on cancel button
+  5: add on card preview value from inputs
+  6: create 'adding payment method'
+
+  INFORMATION:
+  Это редактор существующих карт! и добавление новых,
+  они должны быть списком добавленных методов оплаты
+*/
